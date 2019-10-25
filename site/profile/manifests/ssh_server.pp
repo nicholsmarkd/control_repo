e3f2749767badc2aa9d098ba7d3e3009d29281bd
Mark Nichols <markn.psalm37@gmail.com>
2019-10-25 12:15:05 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCtxmjp5hiBA+KtVsZfwPfo+K7gO/Pb8enZXwBshOoKRo6kgjwxwE6qUBZbGLmIGXi7lzWiM4T25u2/snjxDRzoLXwA1OHCJFhafFN+5dzPkhzjulHc6sevBbtVJGCa4KXVoNK7yNY883OmCid3ORoMc0zgtdKIuLXiiqbf+q9lyLAWgSn19FSkgEnAsKJggy2Khmkpwkr9sbsDIfx3jaYMqQGWC4jP2UOyFPsXwBWyUw4SiMvAcfcBY/eOcnWwb45uLs60mqgIU+fFmBwi+6SwpjBQH4JhcuRtOwjbtY1YzB7eaggSNVLszqirAL04Sac+REGZNf19U6cC8rnO+uBN',
 	}  
 }
